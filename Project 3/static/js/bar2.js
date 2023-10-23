85bd00cabfec1e478fefd1bab1f981f7e5498b86
AGTrainor <136747693+AGTrainor@users.noreply.github.com>
2023-10-23 19:43:38 -0400

    Add files via upload

---
+++ Project 3/static/js/bar2.js
@@ -0,0 +1,30 @@
+d3.json("static/js/data.json").then((data) => {
+    const typeCounts = {}
+
+    data.forEach((entry) => {
+        const type = entry.brewery_type;
+        if (typeCounts[type]) {
+            typeCounts[type] += 1;
+        } else {
+            typeCounts[type] = 1;
+        }
+    });
+
+    const typeNames = Object.keys(typeCounts);
+    const barCounts = Object.values(typeCounts);
+
+    let trace1 = {
+        x: typeNames,
+        y: barCounts,
+        type: "bar"
+    };
+
+    let layout = {
+        title: "Number of Breweries by Type"
+    }
+
+    let plotData = [trace1];
+
+    Plotly.newPlot("simpleplot2", plotData, layout);
+
+});
